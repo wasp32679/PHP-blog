7c5e6d0d8b86f443c0210d3a9657c78a34897ae0
wasp32679 <ryan.kenmoe@outlook.fr>
2026-05-12 08:27:37 +0200

    fix: fixed wrap

--- src/views/posts/detailpost.php
+++ src/views/posts/detailpost.php
@@ -33,7 +33,7 @@
 
         <!--Content-->
         <div class=" w-full p-8 md:p-24 text-xl md:text-2xl text-white leading-normal" style="font-family:Georgia,serif;">
-            <p class="wrap-break-words whitespace-pre-line"><?= htmlspecialchars($post->content, ENT_QUOTES, 'UTF-8') ?></p>
+            <p class="break-all whitespace-pre-line"><?= htmlspecialchars($post->content, ENT_QUOTES, 'UTF-8') ?></p>
         </div>
 
         <!--Author-->
