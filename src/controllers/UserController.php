8fa9fad3e91b89c3b77e36ac52182fec800fc2bc
wasp32679 <ryan.kenmoe@outlook.fr>
2026-05-26 11:00:05 +0200

    fix: properly clear cookies on logout

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -103,8 +103,20 @@ class UserController
 
     public static function logout(): void
     {
+        $_SESSION = [];
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
         session_destroy();
-        setcookie(session_name(), "", time() - 3600);
         header("Location: /");
         exit;
     }
